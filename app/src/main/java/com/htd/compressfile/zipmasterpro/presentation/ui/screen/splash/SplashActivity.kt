eb39723f3970e5770993fd9c9775b9318311b044
DatHoang <htd452@gmail.com>
2023-05-19 22:06:06 +0700

    Logic for splash.

--- app/src/main/java/com/htd/compressfile/zipmasterpro/presentation/ui/screen/splash/SplashActivity.kt
+++ app/src/main/java/com/htd/compressfile/zipmasterpro/presentation/ui/screen/splash/SplashActivity.kt
@@ -1,9 +1,18 @@
 package com.htd.compressfile.zipmasterpro.presentation.ui.screen.splash
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.htd.compressfile.zipmasterpro.databinding.ActivitySplashBinding
+import com.htd.compressfile.zipmasterpro.presentation.ui.MainActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import timber.log.Timber
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -12,6 +21,27 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUp()
     }
-    
+
+    private fun setUp() {
+        startDelay()
+        loading()
+    }
+
+    private fun startDelay() {
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            },
+            3000
+        )
+    }
+
+    private fun loading() {
+    }
+
+
 }
